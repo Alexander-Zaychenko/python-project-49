0284651891daff3a08d5e1b305c6fe86b89c18d2
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-06-30 13:39:09 +0300

    brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,6 @@
 from brain_games.cli import welcome_user
 from random import randint
+from ..engine import engine
 import prompt
 
 
@@ -7,6 +8,7 @@ def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     right_answers = 0
+    wins_for_win = 3
 
     while right_answers < 3:
         question = randint(1, 100)
@@ -16,16 +18,16 @@ def main():
             corr_answer = 'no'
 
         print(f'Question: {question}')
+
+        # CHECK
         answer = prompt.string('Your answer: ')
         if (not question % 2 and answer == corr_answer) or\
                 (question % 2 and answer == corr_answer):
             print('Correct!')
             right_answers += 1
-            continue
-        print(
-            f"'{answer}' is wrong answer ;(. Correct answer was '{corr_answer}'"
-        )
-        print(f"Let's try again, {name}!")
-        break
-    if right_answers == 3:
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{corr_answer}'")
+            print(f"Let's try again, {name}!")
+
+    if right_answers == wins_for_win:
         print(f"Congratulations, {name}!")
