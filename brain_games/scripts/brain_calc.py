d4fd0a0307ea9bc98b644f90cc6851d80ab98bb9
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-07-09 19:39:34 +0300

    changed parameters to engine func

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,7 @@
-from brain_games.cli import welcome_user
 from ..engine import engine
 
 
 def main():
-    name = welcome_user()
     game = 'brain-calc'
-    print('What is the result of the expression?')
-    engine(game, name)
+    engine(game)
 
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,6 @@
-from brain_games.cli import welcome_user
 from ..engine import engine
 
 
 def main():
-    name = welcome_user()
     game = 'brain-even'
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    engine(game, name)
-
+    engine(game)
